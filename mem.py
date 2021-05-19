825b9cd3d1afe684d1a808df88029f9c7ae9bbb6
Danimal <danimal@bikegasm.org>
2021-05-19 16:44:49 -0400

    refactor file management. No longer block on play

--- mem.py
+++ mem.py
@@ -4,6 +4,8 @@
 import queue
 import time
 import sys
+import glob
+import os
 
 # Community - Audio
 import sounddevice as sd
@@ -23,7 +25,7 @@ def main():
 
     pygame.init()
 
-    pygame.display.set_caption('audi-mem')
+    pygame.display.set_caption('oral traadition')
     window_surface = pygame.display.set_mode((800, 600))
 
     background = pygame.Surface((800, 600))
@@ -31,38 +33,73 @@ def main():
 
     is_running = True
 
-    filenames = []
+    file_manager = FileManager(prefix)
 
     play_idx = 0
     while is_running:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 is_running = False
-            elif event.type == pygame.KEYDOWN and event.key == K_r:
-                filenames.append("{}-{}.wav".format(prefix, len(filenames)))
-                print("recording {}...".format(filenames[-1]))
-                rec.start(filenames[-1])
-                play_idx = len(filenames) - 1
             elif event.type == pygame.KEYUP and event.key == K_q:
                 is_running = False
+            elif event.type == pygame.KEYDOWN and event.key == K_r:
+                rec.start(file_manager.new_file())
             elif event.type == pygame.KEYUP and event.key == K_r:
                 rec.stop()
             elif event.type == pygame.KEYDOWN and event.key == K_p:
-                print("playing... {}")
-                play_audio(filenames[play_idx])
+                play_audio(file_manager.current)
             elif event.type == pygame.KEYDOWN and event.key == K_n:
-                play_idx = min(len(filenames) - 1, play_idx + 1)
-            elif event.type == pygame.KEYDOWN and event.key == K_1:
-                play_idx = max(0, play_idx - 1)
+                file_manager.next()
+            elif event.type == pygame.KEYDOWN and event.key == K_b:
+                file_manager.back()
+            elif event.type == pygame.KEYDOWN and event.key == K_s:
+                file_manager.seek(0)
+            elif event.type == pygame.KEYDOWN and event.key == K_e:
+                file_manager.seek(-1)
 
         window_surface.blit(background, (0, 0))
 
         pygame.display.update()
 
 def play_audio(path):
+    sd.stop()
     data, fs = sf.read(path, dtype='float32')
     sd.play(data, fs)
-    sd.wait()
+
+class FileManager:
+
+    def __init__(self, prefix):
+        self.paths = sorted(glob.glob("{}*.wav".format(prefix)))
+        self.idx = 0
+
+    def new_file(self):
+        path = "{}-{}.wav".format(prefix, str(len(self.path) + 1).zfill(5))
+        self.paths.append(path)
+
+        return path
+
+    def seek(self, pos):
+        if pos < 0:
+            self.idx = max(0, self.max_idx + pos)
+        else:
+            self.idx = min(self.max_idx, pos)
+
+    def next(self, count=1):
+        self.idx = min(self.max_idx, self.idx + count)
+
+    def back(self, count=1):
+        self.idx = max(0, self.idx - count)
+
+    @property
+    def current(self):
+        return self.paths[self.idx]
+
+    def remove_current(self):
+        os.remove(self.paths.pop())
+
+    @property
+    def max_idx(self):
+        return len(self.paths) - 1
 
 class Recorder:
 
